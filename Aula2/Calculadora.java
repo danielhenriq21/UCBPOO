eaa6788ec2663d406b85ac7f1325d00efe0e7bcc
danielhenriq21 <danielhenriq.21@gmail.com>
2024-03-16 11:13:30 -0300

    Adiciona.Aula2

---
+++ Aula2/Calculadora.java
@@ -0,0 +1,24 @@
+package Aula2;
+
+public class Calculadora {
+
+    Double numA;
+    Double numB;
+
+    Double somar(){
+        return numA + numB;
+    }
+
+    Double subtrair(){
+        return numA - numB;
+    }
+
+    Double multiplicar(){
+        return numA * numB;
+    }
+
+    Double dividir(){
+        return numA / numB;
+    }
+
+}
---
+++ Aula2/Main.java
@@ -0,0 +1,15 @@
+package Aula2;
+
+public class Main {
+    public static void main(String[] args) { // Digitar Main
+
+        Calculadora calc = new Calculadora(); // Chamada da classe Calculadora como objeto (classe Objeto = novo objeto)
+        calc.numA = 2.0; // Definir valor numA
+        calc.numB = 3.0; // Definir valor numB
+
+        System.out.println("Somar: " + calc.numA + " + " + calc.numB + " = " + calc.somar()); // (string + chamadaDoObjeto.variavel()) 
+        System.out.println("Subtrair: " + calc.numA + " - " + calc.numB + " = " + calc.subtrair()); // Digitar sistout 
+        System.out.println("Multiplicar: " + calc.numA + " x " + calc.numB + " = " + calc.multiplicar());
+        System.out.println("Dividir: " + calc.numA + " / " + calc.numB + " = " + calc.dividir());
+    }
+}
